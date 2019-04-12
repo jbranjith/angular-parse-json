a37620ac007ce16703d35a978fbec553382b7a24
jbranjith <onlineranjithsekar@gmail.com>
2019-04-12 19:53:16 +0530

    1 file modified, 4 added, 2 deleted

--- src/users-sorting/users-sorting.ts
+++ src/users-sorting/users-sorting.ts
@@ -2,46 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatSort, MatTableDataSource } from '@angular/material';
 
-export interface User {
-  login: string,
-  id: number,
-  url: string,
-  html_url: string,
-  type: string,
-  site_admin: boolean
-}
-const USER_DATA: User[] = [
-  {
-    "login": "mojombo",
-    "id": 1,
-    "url": "https://api.github.com/users/mojombo",
-    "html_url": "https://github.com/mojombo",
-    "type": "User",
-    "site_admin": false
-  },
-  {
-    "login": "defunkt",
-    "id": 2,
-    "url": "https://api.github.com/users/defunkt",
-    "html_url": "https://github.com/defunkt",
-    "type": "User",
-    "site_admin": false
-  }, {
-    "login": "pjhyett",
-    "id": 3,
-    "url": "https://api.github.com/users/pjhyett",
-    "html_url": "https://github.com/pjhyett",
-    "type": "User",
-    "site_admin": false
-  },
-  {
-    "login": "wycats",
-    "id": 4,
-    "url": "https://api.github.com/users/wycats",
-    "html_url": "https://github.com/wycats",
-    "type": "User",
-    "site_admin": false
-  }];
+import { User } from './users-sorting-model';
+import * as UserSortingConst from './users-sorting-constants';
+import userDataFromFile from '../assets/github-user-data.json';
 
 @Component({
   selector: 'users-sorting',
@@ -50,7 +13,35 @@ const USER_DATA: User[] = [
 })
 export class UsersSorting implements OnInit {
   displayedColumns: string[] = ['login', 'id', 'url', 'html_url', 'type'];
-  constructor(private http: HttpClient) { }
+  userDataFromApi: User[];
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get<User[]>('https://api.github.com/users')
+      .subscribe((res: User[]) => {
+        //console.log(res);
+        this.userDataFromApi = res;
+      });
+  }
+
+  // from api
+  dataSource = new MatTableDataSource(this.userDataFromApi);
+
+  // from const
+  //dataSource = new MatTableDataSource(UserSortingConst.userDataFromConst);
+
+  // from file
+  // dataSource = new MatTableDataSource(userDataFromFile);
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+
+
+  // OTHER
   public usersArr: User[];
   jsonData = this.http.get('https://api.github.com/users');
 
@@ -58,12 +49,5 @@ export class UsersSorting implements OnInit {
     return this.jsonData.subscribe((users: User[]): any => {
       this.usersArr = users;
     });
-  }
-  //dataSource = new MatTableDataSource(this.usersArr);
-  dataSource = new MatTableDataSource(USER_DATA);
-  @ViewChild(MatSort) sort: MatSort;
-
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
   }
 }
